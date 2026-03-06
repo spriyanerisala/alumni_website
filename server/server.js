e7e552596f9ac5a3f8196a800b4aca12d1f98058
siva priya <nspriya005@gmail.com>
2026-03-06 19:16:02 +0530

    add cors

--- server/server.js
+++ server/server.js
@@ -13,7 +13,10 @@ const app = express()
 const port =process.env.PORT
 
 await connectDB()
-app.use(cors())
+app.use(cors({
+    origin:process.env.CLIENT_URL,
+    credentials:true
+}))
 app.use(express.json())
 
 app.get('/',(req,res)=>{
